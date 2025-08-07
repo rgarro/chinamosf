f63bea44cc28c4e087e21fcaaca3340517b84229
rgarro <rgarro@gmail.com>
2025-08-07 10:43:30 -0600

    rolling on chrome

--- force-app/main/default/lwc/listaComp/listaComp.js
+++ force-app/main/default/lwc/listaComp/listaComp.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class ListaComp extends LightningElement {}
+export default class ListaComp extends LightningElement {
+
+
+    testFunction(){
+        console.log('rolling on chrome');
+    }
+}
